1a5a6084e3d2ecf1145e996b5c3d0a68483abccb
mizonov <mizonov.iv@gmail.com>
2023-01-23 22:20:18 +0500

    Navbar and Slider components added

---
+++ src/components/Navbar.vue
@@ -0,0 +1,51 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="background-color: #00060a">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Логотип</a>
+
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item dropdown">
+            <a class="nav-link active dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Услуги
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="#">Диагностика автомобиля</a></li>
+              <li><a class="dropdown-item" href="#">Ремонт двигателя</a></li>
+              <li><a class="dropdown-item" href="#">Ремонт трансмиссии</a></li>
+              <li><a class="dropdown-item" href="#">Ремонт подвески</a></li>
+              <li><a class="dropdown-item" href="#">Ремонт тормозной системы</a></li>
+              <li><a class="dropdown-item" href="#">Комьютерная диагностика</a></li>
+              <li><a class="dropdown-item" href="#">Шиномонтаж</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">О компании</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Контакты</a>
+          </li>
+        </ul>
+        <a class="navbar-brand" href="#">
+          <img src="/icons/telegram-icon.svg" alt="">
+        </a>
+        <a class="navbar-brand" href="#">
+          <img src="/icons/whatsapp-icon.svg" alt="">
+        </a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
+import '/node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '/node_modules/bootstrap/dist/js/bootstrap.bundle.min'
 import App from './App.vue'
 
 createApp(App).mount('#app')
