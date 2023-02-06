05585f47e2f52dd2f53e0aa6ede81328fa691975
mizonov-iv <mizonov.iv@gmail.com>
2023-02-07 00:07:15 +0500

    Footer update

---
+++ src/components/Footer.vue
@@ -0,0 +1,77 @@
+<template>
+  <footer class="footer mt-auto py-3 text-light footer-wrapper">
+    <div class="container d-flex">
+      <div class="col-10 col-md-6">
+
+        <div id="map" style="width: 100%; height: 400px"></div>
+
+      </div>
+      <div class="col-10 col-md-6">
+        <div class="fs-3 fw-bold">Контакты</div>
+        <div class="d-flex mt-3">
+          <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="rgb(255, 234, 153)" class="bi bi-geo-alt-fill me-2" viewBox="0 0 16 16">
+            <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
+          </svg>
+          <p class="fs-5 fw-bold">Екатеринбург, ул. Раевского, д. 21, корп. А</p>
+        </div>
+        <div class="d-flex mt-3">
+          <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="rgb(255, 234, 153)" class="bi bi-telephone-fill me-2" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+          </svg>
+          <p class="fs-5 fw-bold">+7-922-135-23-45</p>
+        </div>
+        <div class="d-flex mt-3">
+          <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="rgb(255, 234, 153)" class="bi bi-telegram me-2" viewBox="0 0 16 16">
+            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.287 5.906c-.778.324-2.334.994-4.666 2.01-.378.15-.577.298-.595.442-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294.26.006.549-.1.868-.32 2.179-1.471 3.304-2.214 3.374-2.23.05-.012.12-.026.166.016.047.041.042.12.037.141-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8.154 8.154 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629.093.06.183.125.27.187.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.426 1.426 0 0 0-.013-.315.337.337 0 0 0-.114-.217.526.526 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09z"/>
+          </svg>
+          <p class="fs-5 fw-bold">@vservice96</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  created() {
+    let center = [56.854153, 60.669394];
+
+    function init() {
+      let map = new ymaps.Map('map', {
+        center: center,
+        zoom: 15
+      });
+
+      map.controls.remove('geolocationControl'); // удаляем геолокацию
+      map.controls.remove('searchControl'); // удаляем поиск
+      map.controls.remove('trafficControl'); // удаляем контроль трафика
+      map.controls.remove('typeSelector'); // удаляем тип
+      map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+      map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+      map.controls.remove('rulerControl'); // удаляем контрол правил
+      map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+      map.geoObjects.add(new ymaps.Placemark([56.854153, 60.669394], {
+        iconCaption: "VService",
+        iconLayout: 'default#image',
+        iconImageHref: 'https://image.flaticon.com/icons/png/512/64/64113.png',
+        iconImageSize: [40, 40],
+        iconImageOffset: [-19, -44]
+      }))
+    }
+
+    ymaps.ready(init);
+    // ymaps.ready(function () {
+    //   new ymaps.Map("map", {
+    //     center: [56.854153, 60.669394],
+    //     zoom: 16
+    //   });
+    // });
+  }
+}
+</script>
+
+<style scoped>
+.footer-wrapper {
+  background-color: #101112;
+}
+</style>
