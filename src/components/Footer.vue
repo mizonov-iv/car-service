35bdf3216ce01411e117d243de5a3dee8dc0818d
ilya <mizonov.iv@gmail.com>
2023-02-16 20:15:54 +0500

    16.02 update

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -10,7 +10,7 @@
           <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="rgb(255, 234, 153)" class="bi bi-geo-alt-fill me-2" viewBox="0 0 16 16">
             <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
           </svg>
-          <p class="fs-5 fw-bold font-medium">Екатеринбург, <br class="d-md-none"> ул. Раевского, д. 21, корп. А</p>
+          <p class="fs-5 fw-bold font-medium">Екатеринбург, <br class="d-md-none"> ул. Раевского, <br class="d-md-none">д. 21, корп. А</p>
         </div>
         <div class="d-flex mt-3">
           <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="rgb(255, 234, 153)" class="bi bi-telephone-fill me-2" viewBox="0 0 16 16">
